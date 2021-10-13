f402f64cfcd11a587955e84280669b58a022c781
hiroki kinoshita <hiroki.kinoshita@sun-asterisk.com>
2021-10-14 00:20:49 +0900

    chapter 9

---
+++ functions/index.ts
@@ -0,0 +1,18 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
+
+export const fetchBookInfo = functions.https.onRequest(
+  async (request, response) => {
+    try {
+      const db = admin.firestore();
+      const doc = await db.collection("books").doc("000-0000000000").get();
+
+      const bookInfo = doc.data();
+      response.send(bookInfo);
+    } catch (e) {
+      console.error(e);
+      response.status(500).send(e);
+    }
+  }
+);
